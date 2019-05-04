cdf685a60013f9906266c04b1d9215c025ff8874
Ivan <strange.mole@gmail.com>
2019-05-04 05:48:51 +0300

    check

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import 'style/index.css'
 import App from 'components/App/App'
 import * as serviceWorker from './serviceWorker'
 
-const bolt = ''
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
