2c6a5b528dd83865a50b8368fd9b622308b97d8b
Ivan <strange.mole@gmail.com>
2019-05-04 03:49:51 +0300

    change structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from 'images/logo.svg'
+import style from './App.module.css'
+
+const App: React.FC = () => {
+  return (
+    <div className={style.app}>
+      <header className={style.header}>
+        <img src={logo} className={style.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className={style.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
