bb66046a8ac666a6cc481e2938f91fd4bd54117d
Ivan <strange.mole@gmail.com>
2019-05-05 09:34:36 +0300

    font

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import 'react-hot-loader'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'styles/index.css'
 import { Provider } from 'react-redux'
-
-import 'style/index.css'
-
+import FontFaceObserver from 'fontfaceobserver'
 import App from 'components/App/App'
 import { configureStore } from 'store/configureStore'
 import * as serviceWorker from './serviceWorker'
+
+const robotoObserver = new FontFaceObserver('Roboto', {})
+
+robotoObserver.load().then(() => {
+  document.body.classList.add('font-loaded')
+})
 
 const store = configureStore()
 
